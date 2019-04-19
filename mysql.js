3cf319bfd10cb8ad2cecb2aba3a5c0ec0e7d7b09
wuxiaolin <wuxiaolin@gaosiedu.com>
2019-04-19 15:32:20 +0800

    Add sql.

--- mysql.js
+++ mysql.js
@@ -5,10 +5,23 @@ const cfg = {
   password: "wxl171988",
   database: "test"
 };
+//创建sql
+const INSERT_SQL = `INSERT INTO goods VALUES(?,?,?)`;
+const SELECT_SQL = `SELECT * FROM goods`;
 //创建连接对象
-const conn = mysql.createConnection();
+const conn = mysql.createConnection(cfg);
 conn.connect(err => {
-  if (err) {
-    throw err;
-  }
+  if (err) throw err;
+  console.log("连接成功");
+  // conn.query(SELECT_SQL, (err, data) => {
+  //   console.log("查询结果：", data);
+  // });
+  conn.query(INSERT_SQL, [5, "草莓", 80], (err, results) => {
+    if (err) throw err;
+    console.log("插入结果：", results);
+    conn.query(SELECT_SQL, (err, results) => {
+      console.log("查询结果：", results);
+      conn.end();
+    });
+  });
 });
